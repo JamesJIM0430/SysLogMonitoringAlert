57984c08c9dec8eec332781baa7540b16d194dba
JamesJIM0430 <76642657+JamesJIM0430@users.noreply.github.com>
2026-04-28 21:42:54 -0700

    Update main.py
    Added threshold-based alert system for failed login attempts

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ def detect_suspicious_activity(log_file):
         "authentication failure"
     ]
 
+    failed_count = 0
+    threshold = 3  # alert if 3 or more attempts
+
     with open(log_file, "r") as file:
         for line in file:
             for keyword in suspicious_keywords:
                 if keyword in line:
-                    print("[ALERT] Suspicious activity detected:")
-                    print(line.strip())
+                    failed_count += 1
 
-                    with open("alerts.txt", "a") as alert_file:
-                        alert_file.write(line)
+                    if failed_count >= threshold:
+                        print("[ALERT] Multiple failed login attempts detected!")
+                        print(line.strip())
+
+                        with open("alerts.txt", "a") as alert_file:
+                            alert_file.write(line)
 
 
 if __name__ == "__main__":
